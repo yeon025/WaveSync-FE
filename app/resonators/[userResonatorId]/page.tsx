a3359610f666659128d5b00f29c8b08322d2edcb
yeon025 <soyeon6527@gmail.com>
2026-07-15 05:15:12 +0900

    style: 상세 페이지 위치 변경

--- app/resonators/[userResonatorId]/page.tsx
+++ app/resonators/[userResonatorId]/page.tsx
@@ -23,7 +23,7 @@ export default async function Page({ params }: Props) {
       <DesktopSidebar active="resonator-info" userResonatorId={userResonatorId} />
 
       {/* 캐릭터 이미지 */}
-      <div className="relative flex justify-center lg:pointer-events-none lg:absolute lg:left-1/2 lg:z-0 lg:-translate-x-1/2">
+      <div className="relative left-1/2 z-0 flex -translate-x-1/2 justify-center lg:absolute lg:mt-10">
         <img
           src={resonatorDetail.standingImageUrl}
           alt={resonatorDetail.resonatorName}
@@ -33,21 +33,18 @@ export default async function Page({ params }: Props) {
         />
       </div>
 
-      {/* 좌우 레이아웃 */}
-      <div className="mx-[5vw] grid grid-cols-1 gap-[4vh] lg:ml-[10vw] lg:grid-cols-[clamp(250px,22vw,400px)_1fr_clamp(350px,20vw,400px)] lg:gap-0 lg:pt-[8vh]">
-        {/* 좌측 정보 */}
-        <div className="order-2 lg:order-1">
+      {/* 좌우 정보 */}
+      <div className="relative z-10 mx-[5vw] grid grid-cols-1 gap-[4vh] lg:mx-[10vw] lg:flex lg:justify-between lg:pt-[8vh]">
+        <div className="lg:w-[clamp(250px,22vw,400px)]">
           <ResonatorInfo resonator={resonatorDetail} />
         </div>
 
-        {/* 중앙 빈 영역 - 이미지 자리 */}
-        <div className="hidden lg:order-2 lg:block" />
-
-        {/* 우측 정보 */}
-        <div className="order-3 flex flex-col gap-8 lg:w-full">
+        <div className="lg:w-[clamp(350px,20vw,400px)]">
           <WeaponCard weapon={resonatorDetail.weapon} />
 
-          <ResonanceChain level={resonatorDetail.resonanceChainLevel} />
+          <div className="mt-8">
+            <ResonanceChain level={resonatorDetail.resonanceChainLevel} />
+          </div>
         </div>
       </div>
 
